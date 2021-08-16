d1ffac0e07d570aa8712cea0a6505816ecbf89d3
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-08-16 11:18:09 -0300

    Creating form and saving user

--- src/app/modules/users/pages/users/users.component.ts
+++ src/app/modules/users/pages/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/http/user.service';
 import { ModalService } from 'src/app/shared/components/modal/modal.service';
-import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -9,29 +9,50 @@ import { User } from 'src/app/shared/models/user.model';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-
+  form: FormGroup;
 
   constructor(
     private userService: UserService,
     private modalService: ModalService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      login: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required),
+    });
+  }
 
   save(): void {
-    this.userService
-      .save(
-        new User({
-          name: 'Felipe Gabriel Pinheiro',
-          login: 'felipe',
-          password: '123',
-          email: 'teste@yahoo.com.br',
-        })
-      )
-      .subscribe();
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const user = this.form.value;
+
+    this.userService.save(user).subscribe((res) => {
+      console.log(res);
+      this.closeModal();
+    });
   }
 
   openModal(): void {
+    this.form.reset();
     this.modalService.open('modal');
+  }
+
+  closeModal(): void {
+    this.modalService.close('modal');
+  }
+
+  showErrorMessage(field: string): boolean {
+    return this.form.get(field).invalid && this.form.get(field).touched;
   }
 }
